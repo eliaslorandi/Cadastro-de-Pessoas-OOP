bfd62eebc6ce0017db4ae9560ddcf351519eb54a
Elias Lorandi <elias.lorandi@hotmail.com>
2023-11-02 14:49:10 -0300

    Correção de bugs
    Correção de bugs

--- pessoa_list.php
+++ pessoa_list.php
@@ -20,8 +20,14 @@
     <tbody>
         <?php
             $conn = mysqli_connect('localhost', 'root', '', 'livro');
+            if ( !empty($_GET['action']) AND $_GET['action'] == 'delete' )
+            {
+                $id = (int) $_GET['id'];
+                $result = mysqli_query($conn, "DELETE FROM pessoas WHERE id = '{$id}'");
+
+            }
             $result = mysqli_query($conn, 'SELECT * from pessoas ORDER BY id');
-            
+
             while ($row = mysqli_fetch_assoc($result))
             {
                 $id        = $row['id'];
@@ -33,10 +39,10 @@
                 $id_cidade = $row['id_cidade'];
                 
                 print '<tr>';
-                print "<td> <a href='pessoa_form_edit.php?id={$id}'>
+                print "<td> <a href='pessoa_form.php?action=edit&id={$id}'>
                              <img src='images/edit.svg' style='width:17px'>
                             </a> </td>";
-                print "<td> <a href='pessoa_delete.php?id={$id}'>
+                print "<td> <a href='pessoa_list.php?action=delete&id={$id}'>
                              <img src='images/remove.svg' style='width:17px'>
                             </a> </td>";
                 print "<td> {$id} </td>";
@@ -50,7 +56,7 @@
     </tbody>
     </table>
     
-    <button onclick="window.location='pessoa_form_insert.php'">
+    <button onclick="window.location='pessoa_form.php'">
         <img src="images/insert.svg" style="width:17px"> Inserir
     </button>
     
