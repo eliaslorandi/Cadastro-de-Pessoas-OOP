b66672aa72037fcf453d730cbe0f75d76f3b8417
Elias Lorandi <elias.lorandi@hotmail.com>
2023-11-04 14:14:36 -0300

    Separação php e db
    Separação php e db

--- pessoa_list.php
+++ pessoa_list.php
@@ -1,21 +1,25 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', '', 'livro');
+require_once 'database/pessoa_db.php';
+
 if (!empty($_GET['action']) and $_GET['action'] == 'delete') {
     $id = (int) $_GET['id'];
-    $result = mysqli_query($conn, "DELETE FROM pessoas WHERE id = '{$id}'");
+    excluir_pessoa($id);
 }
-$result = mysqli_query($conn, 'SELECT * from pessoas ORDER BY id'); //carrega as pessoas do db
+$pessoas = lista_pessoas();
 
 $items = '';
-while ($row = mysqli_fetch_assoc($result)) { //percorro os registros
-    $item = file_get_contents('html/item.html'); //para cada registro, leio o item.html e substituo as variaveis do modelo
-    $item = str_replace('{id}', $row['id'], $item);
-    $item = str_replace('{nome}', $row['nome'], $item);
-    $item = str_replace('{endereco}', $row['endereco'], $item);
-    $item = str_replace('{bairro}', $row['bairro'], $item);
-    $item = str_replace('{telefone}', $row['telefone'], $item);
 
-    $items .= $item;
+if ($pessoas) {
+    foreach ($pessoas as $pessoa) {
+        $item = file_get_contents('html/item.html'); //para cada registro, leio o item.html e substituo as variaveis do modelo
+        $item = str_replace('{id}', $pessoa['id'], $item);
+        $item = str_replace('{nome}', $pessoa['nome'], $item);
+        $item = str_replace('{endereco}', $pessoa['endereco'], $item);
+        $item = str_replace('{bairro}', $pessoa['bairro'], $item);
+        $item = str_replace('{telefone}', $pessoa['telefone'], $item);
+
+        $items .= $item;
+    }
 }
 
 $list = file_get_contents('html/list.html'); //leio o list.html
