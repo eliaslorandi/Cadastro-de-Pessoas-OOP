f897ce488b3b80b8ddf7dd847aa657924f361505
Elias Lorandi <elias.lorandi@hotmail.com>
2023-11-14 10:55:31 -0300

    Correção de erros no código

--- classes/Pessoa.php
+++ classes/Pessoa.php
@@ -3,7 +3,7 @@ class Pessoa
 {
     private static $conn; //static para manter o valor inicial
 
-    public static function getConneciton()
+    public static function getConnection()
     {
         if (empty(self::$conn)) { //if para nao abrir a conexao só vez, se estiver aberta, usa a mesma conexão
             $ini = parse_ini_file('config/livro.ini');
@@ -20,7 +20,7 @@ class Pessoa
 
     public static function find($id)
     {
-        $conn = self::getConneciton();
+        $conn = self::getConnection();
 
         $result = $conn->prepare("SELECT * FROM pessoas WHERE id=:id"); //prepare para evitar sql injection
         $result->execute([':id' => $id]); //indica a variável para a posição do parametro
@@ -29,7 +29,7 @@ class Pessoa
 
     public static function delete($id)
     {
-        $conn = self::getConneciton();
+        $conn = self::getConnection();
 
         $result = $conn->prepare("DELETE FROM pessoas WHERE id=:id");
         $result->execute([':id' => $id]);
@@ -37,7 +37,7 @@ class Pessoa
 
     public static function all()
     {
-        $conn = self::getConneciton();
+        $conn = self::getConnection();
 
         $result = $conn->query("SELECT * FROM pessoas ORDER BY id"); //all não precisa de prepare pois não tem injeção de parametro do usuario
         return $result->fetchAll();
@@ -45,7 +45,7 @@ class Pessoa
 
     public static function save($pessoa)
     {
-        $conn = self::getConneciton();
+        $conn = self::getConnection();
 
         if (empty($pessoa['id'])) {
             $result = $conn->query("SELECT max(id) as next FROM pessoas");
@@ -53,25 +53,25 @@ class Pessoa
             $pessoa['id'] = (int)$row['next'] + 1;
 
             $sql = "INSERT INTO pessoas (id, nome, endereco, bairro, telefone, email, id_cidade)
-                    VALUES (':id', ':nome', ':endereco', ':bairro', ':telefone', ':email', ':id_cidade')";
+                    VALUES (:id, :nome, :endereco, :bairro, :telefone, :email, :id_cidade)";
         } else {
-            $sql = "UPDATE pessoas SET nome     = :nome,
-                                      endereco  = :endereco,
-                                      bairro    = :bairro,
-                                      telefone  = :telefone,
-                                      email     = :email,
-                                      id_cidade = :id_cidade,
-                                WHERE id  = :id";
+            $sql = "UPDATE pessoas SET nome      = :nome,
+                                       endereco  = :endereco,
+                                       bairro    = :bairro,
+                                       telefone  = :telefone,
+                                       email     = :email,
+                                       id_cidade = :id_cidade
+                                WHERE id = :id";
         }
 
         $result = $conn->prepare($sql);
         $result->execute([
-            ':id' => $pessoa['id'],
-            ':nome' => $pessoa['nome'],
-            ':endereco' => $pessoa['endereco'],
-            ':bairro' => $pessoa['bairro'],
-            ':telefone' => $pessoa['telefone'],
-            ':email' => $pessoa['email'],
+            ':id'        => $pessoa['id'],
+            ':nome'      => $pessoa['nome'],
+            ':endereco'  => $pessoa['endereco'],
+            ':bairro'    => $pessoa['bairro'],
+            ':telefone'  => $pessoa['telefone'],
+            ':email'     => $pessoa['email'],
             ':id_cidade' => $pessoa['id_cidade']
         ]);
     }
